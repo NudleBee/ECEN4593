6be59b03f54e5c8014ceccb244468a7edf720e70
napo3943 <napo3943@colorado.edu>
2017-04-24 18:09:12 -0600

    Initialization

---
+++ cmake-build-debug/CMakeFiles/ECEN4593.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/NathanPottorff/Documents/College/Senior/CompOrg/FinalProject/ECEN4593/main.cpp" "/Users/NathanPottorff/Documents/College/Senior/CompOrg/FinalProject/ECEN4593/cmake-build-debug/CMakeFiles/ECEN4593.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ECEN4593.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ECEN4593.dir/main.cpp.o"
+  "ECEN4593.pdb"
+  "ECEN4593"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ECEN4593.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
